3cca750b297df3605917b80177c41c93da4b7d67
Henrik Weseloh <weselohhenrik@gmail.com>
2021-12-02 11:20:01 +0100

    Part2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,23 +1,36 @@
 import java.io.File
 
 fun main() {
-    val position = Vec2(0, 0)
+    val position1 = Vec2(0, 0)
+    val position2 = Vec2(0, 0)
+    var aim = 0
     val input: List<String> = File("src/Day02.txt").readLines()
 
     for (i in 0.until(input.size)) {
         val tmpList = input[i].split(" ")
 
         when (tmpList[0]) {
-             "forward" -> position.x += tmpList[1].toInt()
-             "down" -> position.y += tmpList[1].toInt()
-             "up" -> position.y -= tmpList[1].toInt()
+             "forward" -> position1.x += tmpList[1].toInt()
+             "down" -> position1.y += tmpList[1].toInt()
+             "up" -> position1.y -= tmpList[1].toInt()
              else -> println("invalid direction")
         }
+
+        when (tmpList[0]) {
+            "down" -> aim += tmpList[1].toInt()
+            "up" -> aim -= tmpList[1].toInt()
+            "forward" -> {
+                position2.x += tmpList[1].toInt()
+                position2.y += (aim * tmpList[1].toInt())
+            }
+            else -> println("invalid direction")
+        }
         println(tmpList)
-        println(position)
+        println(position1)
     }
 
-    println(position.x * position.y)
+    println(position1.x * position1.y)
+    println(position2.x * position2.y)
 
 }
 
